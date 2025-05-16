e1cc2426d8674ae97701a42b029db85f54efbb03
Ndiaye <yousndiaye926@gmail.com>
2025-05-17 00:00:26 +0200

    Sauvegarde avant pull

--- lib/widgets/custom_loader.dart
+++ lib/widgets/custom_loader.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
 import '../utils/colors.dart';
 
@@ -16,7 +15,8 @@ class CustomLoader extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation<Color>(AppColor.primaryColor), // rouge de l’app
+              valueColor: AlwaysStoppedAnimation<Color>(AppColor.primaryColor),
+              // rouge de l’app
               strokeWidth: 4,
             ),
             const SizedBox(height: 20),
@@ -29,36 +29,4 @@ class CustomLoader extends StatelessWidget {
       ),
     );
   }
-=======
-import 'package:flutter/material.dart';
-import '../utils/colors.dart';
-
-class CustomLoader extends StatelessWidget {
-  const CustomLoader({super.key, this.message});
-
-  final String? message;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black, // fond noir pour s'adapter à l'app
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation<Color>(AppColor.primaryColor), // rouge de l’app
-              strokeWidth: 4,
-            ),
-            const SizedBox(height: 20),
-            Text(
-              message ?? 'Chargement...',
-              style: const TextStyle(color: Colors.white, fontSize: 16),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
->>>>>>> 2d7b1fa (Ajout de score questionnaire sur firestore et affichage des scores sur le graphe)
 }
