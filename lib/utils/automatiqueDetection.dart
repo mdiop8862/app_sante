6b16d8a2c8970c4eb272bfc4acd6099ea899cd8b
Moussa Diop <moussa.diop@etu.unilim.fr>
2025-05-19 21:53:10 +0200

    modif

--- lib/utils/automatiqueDetection.dart
+++ lib/utils/automatiqueDetection.dart
@@ -13,7 +13,7 @@ String normalizeTestKey(String title) {
   if (title.contains('flamand')) return 'test_du_flamand';
   if (title.contains('marche') && title.contains('6')) return 'test_de_marche__6_minutes';
   if (title.contains('montée')) return 'test_de_la_montée_de_marche';
-  if (title.contains('assis-debout')) return 'test_du_assis-debout_-_30_sec';
+  if (title.contains('assis-debout')) return 'test_du_assis_debout_30_sec';
   if (title.contains('chaise')) return 'test_de_la_chaise';
   if (title.contains('handgrip')) return 'test_de_handgrip';
   if (title.contains('flexomètre')) return 'test_de_flexomètre';
